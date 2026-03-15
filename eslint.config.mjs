1e96689be4ce92e4d895e644326b9dcf6705e8e6
Volodymyr Koshel <vladimir.kshl@gmail.com>
2026-03-15 15:00:17 +0200

    Installing playwright, eslint
    - Added the Playwright
    - Added and set up the eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+    ignores: ['playwright-report/*', 'test-results/*'],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@stylistic': stylistic,
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-empty-pattern': ['error', { allowObjectPatternsAsParameters: true }],
+      '@typescript-eslint/consistent-indexed-object-style': 'off',
+      // Formatting rules
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/space-in-parens': ['error', 'never'],
+      'eol-last': ['error', 'always'],
+      indent: 'off', // Disable base rule
+      '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', { arrays: 'always-multiline', objects: 'always-multiline', imports: 'always-multiline', exports: 'always-multiline', functions: 'never' }],
+      'object-curly-spacing': 'off', // Disable base rule
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/type-annotation-spacing': 'error',
+      'array-bracket-spacing': ['error', 'never'],
+      '@stylistic/comma-spacing': ['error', { before: false, after: true }],
+      'max-len': [
+        'error',
+        {
+          code: 140,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+    },
+  },
+];
