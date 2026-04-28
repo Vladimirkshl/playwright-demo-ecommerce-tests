f9ecd4ad47aec67bc22eaefc9912af1d11a86af8
Volodymyr Koshel <vladimir.kshl@gmail.com>
2026-04-28 18:40:50 +0300

    feat(product-api): add create product request
    - Added the create product API method by the POST request
    - Added the Product service endpoint
    - Refactored the IProduct interface, added the separate ILaptopCharacteristics interface and the characteristics property

--- api/product-api.ts
+++ api/product-api.ts
@@ -1,5 +1,39 @@
 import { ApiBase } from '@api/base';
+import { PRODUCT_SERVICE_API } from '@constants/api/product-service';
+import { IProduct } from '@constants/solomono/product';
+import { Report } from '@utils/report';
 
 export class ProductApi extends ApiBase {
   apiName = 'ProductApi';
+
+  async create(product: IProduct): Promise<IProduct> {
+    return await Report.subStep(`API: Create Laptop: ${product.name}`, async () => {
+      const response = await this.POST({
+        resource: PRODUCT_SERVICE_API.products,
+        data: {
+          name: product.name,
+          image: product.image,
+          code: product.code,
+          category: product.category,
+          price: product.price,
+          qty: product.qty,
+          shippingDescription: product.shippingDescription,
+          status: product.status,
+          label: product.label,
+          description: {
+            fullDescription: product.description,
+            shortDescription: product.shortDescription,
+          },
+          characteristics: product.characteristics,
+        },
+      });
+
+      // TODO: Send the POST review request
+      // TODO: Merge the review response/data to the product object
+
+      product.api.id = response.api.id;
+
+      return product;
+    });
+  }
 }
---
+++ constants/api/product-service.ts
@@ -0,0 +1,5 @@
+const PRODUCT_SERVICE_ENDPOINT = 'productservice';
+
+export const PRODUCT_SERVICE_API = {
+  products: `${PRODUCT_SERVICE_ENDPOINT}/product`,
+};
