0d052890aa884fd6f7c7ac79c8903afe4359c57b
Volodymyr Koshel <vladimir.kshl@gmail.com>
2026-04-29 23:51:58 +0300

    feat(api): add attach files reviews methods
    - Added the attachFiles and leaveReviews api methods to the create product method

--- api/product-api.ts
+++ api/product-api.ts
@@ -1,6 +1,10 @@
+import playwright from 'playwright';
+
 import { ApiBase } from '@api/base';
 import { PRODUCT_SERVICE_API } from '@constants/api/product-service';
-import { IProduct } from '@constants/solomono/product';
+import { IFile } from '@constants/files/files';
+import { IProduct, IReview, IReviewFile } from '@constants/solomono/product';
+import { FileSystemUtils } from '@utils/file-system-utils';
 import { Report } from '@utils/report';
 
 export class ProductApi extends ApiBase {
@@ -28,12 +32,104 @@ export class ProductApi extends ApiBase {
         },
       });
 
-      // TODO: Send the POST review request
-      // TODO: Merge the review response/data to the product object
-
       product.api.id = response.api.id;
+
+      await this.leaveReviews(product);
+      await this.attachFiles(product);
 
       return product;
     });
   }
+
+  private async leaveReviews(product: IProduct) {
+    for (const review of product.reviews) {
+      await this.leaveReview(product.api.id, review);
+    }
+  }
+
+  private async leaveReview(productId: string, review: IReview) {
+    const response = await this.POST({
+      resource: PRODUCT_SERVICE_API.reviews(productId),
+      data: {
+        name: review.name,
+        comment: review.comment,
+        rating: review.rating,
+        date: review.date.fullDateTime,
+      },
+    });
+    
+    review.id = response.id;
+  }
+
+  private async attachFiles(product: IProduct) {
+    for (const review of product.reviews) {
+      await Report.subStep(`API: Attach file: ${review.file}`, async () => {
+        const signedUrl = await this.getSignedUrl(review.file);
+        await this.sendFileToS3(review.file, signedUrl.signedUrl);
+        review.reviewAttachment = await this.attachFile(review.id, review.file, signedUrl);
+      });
+
+      Report.attachJson('Review attached file', review.file);
+    }
+  }
+
+  private async getSignedUrl(file: IFile): Promise<ISignedUrl> {
+    return (
+      await this.POST({
+        resource: PRODUCT_SERVICE_API.files.signedUrl,
+        data: {
+          fileName: file.fileName,
+          fileExtension: `.${file.fileName.split('.').pop()}`,
+          fileContentLength: file.contentLength,
+        },
+        name: `${this.apiName} / Get signed URL for file`,
+      })
+    ).data as ISignedUrl;
+  } 
+
+  private async sendFileToS3(fileData: IFile, url: string) {
+    const fileContent = FileSystemUtils.readFile(fileData.path, null);
+    const s3RequestContext = await playwright.request.newContext();
+    try {
+      await s3RequestContext.put(url, {
+        data: fileContent,
+        headers: {
+          'Content-Type': fileData.contentType,
+          'Content-Length': fileData.contentLength.toString(), 
+        },
+      });
+    } finally {
+      await s3RequestContext.dispose();
+    }
+  }
+
+  private async attachFile(
+    reviewId: string,
+    file: IFile,
+    signedUrl: ISignedUrl 
+  ): Promise<IReviewFile> {
+    await this.POST({
+      resource: PRODUCT_SERVICE_API.files.attachFile(reviewId),
+      data: {
+        file: {
+          fileName: file.fileName,
+          id: signedUrl.fileId,
+          path: signedUrl.filePath,
+        },
+      },
+      name: `${this.apiName} / Attach file ${file.fileName} to review`,
+    });
+    
+    return {
+      fileId: signedUrl.fileId,
+      fileName: file.fileName,
+      path: signedUrl.filePath,
+    };
+  }
+}
+
+interface ISignedUrl {
+  signedUrl: string;
+  fileId: string;
+  filePath: string;
 }
--- constants/api/product-service.ts
+++ constants/api/product-service.ts
@@ -1,5 +1,15 @@
 const PRODUCT_SERVICE_ENDPOINT = 'productservice';
+const PRODUCTS = `${PRODUCT_SERVICE_ENDPOINT}/products`;
+const REVIEW_SERVICES = `${PRODUCT_SERVICE_ENDPOINT}/review`;
+const REVIEW_ID = (id: string) => `${PRODUCTS}/${id}`;
 
 export const PRODUCT_SERVICE_API = {
   products: `${PRODUCT_SERVICE_ENDPOINT}/product`,
+
+  reviews: (productId: string) => `${PRODUCT_SERVICE_API.products}/${productId}/reviews`,
+
+  files: { 
+    attachFile: (id: string) => `${REVIEW_ID(id)}/attach-file`,
+    signedUrl: `${REVIEW_SERVICES}/files/signed-url`, 
+  },
 };
